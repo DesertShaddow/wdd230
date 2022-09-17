f31199049761a0cb4e34fde5c15d7a77d0bf89f4
DesertShaddow <desertshaddow@gmail.com>
2022-09-17 15:55:45 -0700

    Update with last mod, dynamic cr, and photo up

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,2 +1,5 @@
 const options =  {year: 'numeric'};
 document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', options);
+
+let text = "This document was last modified" + " " + document.lastModified;
+document.getElementById("modified").innerHTML = text;
